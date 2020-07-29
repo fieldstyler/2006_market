72352ea91fe5c04afb1f8453485a6890ab88475b
fieldstyler <tdfields93@gmail.com>
2020-07-29 09:08:28 -0600

    Add initial requires and test_it_exists

---
+++ test/vendor_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/vendor"
+
+class VendorTest < Minitest::Test
+
+  def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_instance_of Vendor, vendor
+  end
+
+  #def test_it_has_attributes
+  #end
+
+end
