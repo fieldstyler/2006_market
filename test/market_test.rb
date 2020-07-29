763e8a28c68201e02e0d8af369a6e45642e293fe
fieldstyler <tdfields93@gmail.com>
2020-07-29 09:43:04 -0600

    Add requires and test_it_exists for market_test and test passes

---
+++ test/market_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/vendor"
+require "./lib/item"
+require "./lib/market"
+
+class MarketTest < Minitest::Test
+
+  def test_it_exists
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_instance_of Market, market
+  end
+
+end 
